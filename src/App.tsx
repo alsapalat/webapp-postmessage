1989432d598a0a2ecf249cc2d92707586909e5ef
alsapalat <alsapalat@multisyscorp.com>
2024-03-04 17:38:47 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import { Children, useEffect, useRef, useState } from 'react';
+import './App.css'
+
+function App() {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [logs, setLogs] = useState<string[]>([]);
+  useEffect(() => {
+    const receive = (e: MessageEvent) => {
+      setLogs((l) => ([`<<RECEIVED>>|${new Date().toISOString()}|${JSON.stringify({ data: e.data })}`]).concat(l));
+    };
+    window.addEventListener('message', receive, false);
+    return () => {
+      window.removeEventListener('message', receive);
+    }
+  }, []);
+  return (
+    <div>
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        window.postMessage(inputRef.current!.value || 'Hello World', '*')
+        setLogs(([`<<SEND>>|${new Date().toISOString()}|${inputRef.current!.value || 'Hello World'}`]).concat(logs))
+      }}><input style={{ width: '200px' }} ref={inputRef} placeholder="Enter Body (Hello World)" /><button type="submit">Send Message</button></form>
+      <hr />
+      <div>Logs:</div>
+      <div style={{ textAlign: 'left', height: '400px', overflow: 'auto' }}>
+        <ul>
+          {Children.toArray(logs.map((row) => <li>{row}</li>))}
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default App
