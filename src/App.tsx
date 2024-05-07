ab5fd206d28a5957283c4a8ceb3df513e8fd94bb
alsapalat <alsapalat@multisyscorp.com>
2024-05-07 15:48:31 +0800

    update version

--- src/App.tsx
+++ src/App.tsx
@@ -171,7 +171,7 @@ function App() {
         </div>
         <div className="px-6 pb-6">
           <PrintButton onPrint={setIsPrinting} />
-          <div className="text-center text-xs mt-4">v1.0.14</div>
+          <div className="text-center text-xs mt-4">Version 1.0.15</div>
         </div>
       </div>
       <EventLogs />
